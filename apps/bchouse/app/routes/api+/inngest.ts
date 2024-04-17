012dcd86e1b136185fdad3800eefb8b631f4f48b
Sahid Miller <sahid.miller@gmail.com>
2024-04-17 13:33:22 -0400

    fix: add back inngest api

---
+++ apps/bchouse/app/routes/api+/inngest.ts
@@ -0,0 +1,10 @@
+import { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node'
+import { inngestService } from '~/.server/services/getContext'
+
+export const loader = (_: LoaderFunctionArgs) => {
+  return inngestService.handler(_)
+}
+
+export const action = (_: ActionFunctionArgs) => {
+  return inngestService.handler(_)
+}
