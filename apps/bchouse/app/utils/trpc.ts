f14a74f786a24c44d8a87ae0db3257232bcb7041
Sahid Miller <sahid.miller@gmail.com>
2024-05-26 20:43:13 -0400

    test: add basic trpc for comparison

--- apps/bchouse/app/utils/trpc.ts
+++ apps/bchouse/app/utils/trpc.ts
@@ -1,3 +1,28 @@
+import { AuthObject } from '@clerk/clerk-sdk-node'
+import { initTRPC } from '@trpc/server'
+import superjson from 'superjson'
+
+export type Context = {
+  auth: AuthObject
+}
+
+/**
+ * Initialization of tRPC backend
+ * Should be done only once per backend!
+ */
+const t = initTRPC.context<Context>().create({
+  transformer: superjson,
+})
+
+/**
+ * Export reusable router and procedure helpers
+ * that can be used throughout the router
+ */
+export const router = t.router
+export const publicProcedure = t.procedure
+export const mergeRouters = t.mergeRouters
+export const createCallerFactory = t.createCallerFactory
+
 import type { DehydratedState } from '@tanstack/react-query'
 import { QueryClient, hydrate } from '@tanstack/react-query'
 import {
@@ -6,8 +31,28 @@ import {
   createTRPCReact,
 } from '@trpc/react-query'
 import type { inferRouterInputs, inferRouterOutputs } from '@trpc/server'
-import superjson from 'superjson'
-import type { AppRouter } from '~/.server/types/router'
+import { z } from 'zod'
+
+export const appRouter = router({
+  test: {
+    a: publicProcedure
+      .input(z.object({ s: z.string(), cursor: z.string().nullish() }))
+      .query(() => {
+        return false
+      }),
+    b: publicProcedure
+      .input(z.object({ s: z.string(), cursor: z.string().nullish() }))
+      .mutation((opts) => {
+        if (opts.input.s === '') {
+          throw new Error('')
+        }
+
+        return false
+      }),
+  },
+})
+
+export type AppRouter = typeof appRouter
 
 export const trpc: CreateTRPCReact<AppRouter, any> =
   createTRPCReact<AppRouter>() as any
@@ -22,7 +67,7 @@ export const createTrpcClientUtils = (dehydratedState: DehydratedState) => {
   const trpcClientUtils = createTRPCQueryUtils<AppRouter>({
     queryClient,
     client: trpc,
-    transformer: superjson,
+    // transformer: superjson,
   })
 
   return trpcClientUtils
