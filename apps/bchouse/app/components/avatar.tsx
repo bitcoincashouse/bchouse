5c45ccdc9a1df174c84c525712510e62ddde4e0b
Sahid Miller <sahid.miller@gmail.com>
2024-05-30 21:07:39 -0400

    fix: image callbacks not firing on hydration

--- apps/bchouse/app/components/avatar.tsx
+++ apps/bchouse/app/components/avatar.tsx
@@ -1,4 +1,5 @@
 import { Fragment, useState } from 'react'
+import { useBrowserLayoutEffect } from '~/utils/useBrowserLayoutEffect'
 import { classNames } from '../utils/classNames'
 import { ImageWithFallbackProps } from './image-with-fallback'
 
@@ -8,54 +9,104 @@ type AvatarProps = Omit<ImageWithFallbackProps, 'fallback'> & {
   square?: boolean
 }
 
-const AvatarWithFallback: React.FC<AvatarProps> = ({
+async function isImageValid(src?: string) {
+  if (!src) return false
+
+  return new Promise((resolve) => {
+    let img = document.createElement('img')
+    img.onerror = () => resolve(false)
+    img.onload = () => resolve(true)
+    img.src = src
+  })
+}
+
+function Fallback({
+  className,
+  square,
+}: {
+  className?: string
+  square?: boolean
+}) {
+  return (
+    <span
+      className={classNames(
+        className,
+        'aspect-[1/1]',
+        !square ? 'rounded-full' : '',
+        `inline-block overflow-hidden bg-gray-100`
+      )}
+    >
+      <svg
+        className="h-full w-full text-gray-300"
+        fill="currentColor"
+        viewBox="0 0 24 24"
+      >
+        <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
+      </svg>
+    </span>
+  )
+}
+
+function reducer(
+  state: any,
+  action: 'IMAGE_LOADED' | 'IMAGE_ERROR' | 'IMAGE_RESET'
+) {
+  if (action === 'IMAGE_LOADED') {
+    return {
+      showFallback: false,
+      hideImage: false,
+    }
+  } else if (action === 'IMAGE_ERROR') {
+    return {
+      showFallback: true,
+      hideImage: false,
+    }
+  } else {
+    return {
+      showFallback: false,
+      hideImage: true,
+    }
+  }
+}
+
+export const Avatar: React.FC<AvatarProps> = ({
   src,
   square,
   className = '',
   ...props
 }) => {
   const [showFallback, setShowFallback] = useState(false)
-  const showAvatar = !showFallback && !!src
+
+  //Allow browser to detect breaking images when SSR (SSR fails to run onError)
+  useBrowserLayoutEffect(() => {
+    isImageValid(src).then((isValid) => {
+      if (!isValid) {
+        setShowFallback(true)
+      }
+    })
+  }, [src])
+
+  const imgClasses = classNames(
+    className,
+    'aspect-[1/1]',
+    !square && 'rounded-full',
+    `flex-none bg-primary/10`
+  )
 
   return (
-    <Fragment>
-      {showAvatar ? (
-        <img
-          {...props}
-          onError={() => setShowFallback(true)}
-          src={src}
-          className={classNames(
-            className,
-            'aspect-[1/1]',
-            !square && 'rounded-full',
-            `flex-none bg-primary/10`
-          )}
-        />
-      ) : (
-        <span
-          className={classNames(
-            className,
-            'aspect-[1/1]',
-            !square ? 'rounded-full' : '',
-            `inline-block overflow-hidden bg-gray-100`
-          )}
-        >
-          <svg
-            className="h-full w-full text-gray-300"
-            fill="currentColor"
-            viewBox="0 0 24 24"
-          >
-            <path d="M24 20.993V24H0v-2.996A14.977 14.977 0 0112.004 15c4.904 0 9.26 2.354 11.996 5.993zM16.002 8.999a4 4 0 11-8 0 4 4 0 018 0z" />
-          </svg>
-        </span>
-      )}
-    </Fragment>
-  )
-}
-export const Avatar: React.FC<AvatarProps> = ({ src, ...props }) => {
-  return (
     <Fragment key={src}>
-      <AvatarWithFallback {...props} src={src} />
+      {!showFallback ? (
+        <>
+          <img
+            {...props}
+            onError={() => setShowFallback(true)}
+            src={src}
+            className={imgClasses}
+          />
+        </>
+      ) : (
+        <Fallback className={className} square={square} />
+      )}
     </Fragment>
   )
 }
